2c21b5d98094f49ec0be524ef5ebe8e6147a2436
VegaSamuel <samuelvega.122003@gmail.com>
2025-03-20 16:27:40 -0700

    Todo hecho en clase 20/03

---
+++ app/src/main/java/itson/edu/mx/firebasestorage_233094/RegistrarPokemon.kt
@@ -0,0 +1,28 @@
+package itson.edu.mx.firebasestorage_233094
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class RegistrarPokemon : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_registrar_pokemon)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val name: EditText = findViewById(R.id.etName)
+        val number: EditText = findViewById(R.id.etNumber)
+        val upload: Button = findViewById(R.id.btnUploadImage)
+        val save: Button = findViewById(R.id.btnSavePokemon)
+    }
+}
